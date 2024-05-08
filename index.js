b6a62ee14c5eac04b5301f648c86377d3c3aeb51
Mukesh Kaushal <imukeshkaushal@gmail.com>
2024-05-08 10:33:47 +0530

    file added

--- index.js
+++ index.js
@@ -27,30 +27,39 @@ app.get('/', (req,res) => {
     res.send("Welcome to Node Js With Wordpress");
 })
 
-// Registration endpoint
+// Registration 
 app.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
-  
-    try {
-      const checkUserQuery = 'SELECT * FROM wp_users WHERE user_login = ?';
-      const existingUser = await queryDB(checkUserQuery, [username]);
-      if (existingUser.length > 0) {
-        return res.status(400).json({ error: 'User already exists' });
-      }
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const insertUserQuery = 'INSERT INTO wp_users (user_login, user_pass, user_email) VALUES (?, ?, ?)';
-      await queryDB(insertUserQuery, [username, hashedPassword, email]);
-  
-      const user = { user_login: username, user_email: email }; 
-      const token = await loginUser(user);
-  
-      return res.status(200).json({ message: 'User registered successfully', token, user });
-    } catch (error) {
-      console.error('Error registering user:', error);
-      return res.status(500).json({ error: 'Internal server error' });
+  const { username, email, password } = req.body;
+
+  try {
+    const checkUserQuery = 'SELECT * FROM wp_users WHERE user_login = ?';
+    const existingUser = await queryDB(checkUserQuery, [username]);
+    if (existingUser.length > 0) {
+      return res.status(400).json({ error: 'User already exists' });
     }
-  });
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const insertUserQuery = 'INSERT INTO wp_users (user_login, user_pass, user_email) VALUES (?, ?, ?)';
+    await queryDB(insertUserQuery, [username, hashedPassword, email]);
+    
+    const getUserDetailsQuery = 'SELECT * FROM wp_users WHERE user_login = ?';
+    const newUser = await queryDB(getUserDetailsQuery, [username]);
+
+    const user = {
+      id: newUser[0].ID,
+      username: newUser[0].user_login,
+      email: newUser[0].user_email,
+
+    };
+    const token = await loginUser(user);
+
+    return res.status(200).json({ message: 'User registered successfully', token, user });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
   
   // Login function
   async function loginUser(user) {
